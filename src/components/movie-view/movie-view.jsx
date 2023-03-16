d53bee0c2a85939c7965518b2e6dbf56b4381884
JackOliCode <jack.v.oliver@gmail.com>
2023-03-16 00:20:28 +0000

    added w-100 to image in movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,7 +4,7 @@ export const MovieView = ({ movie, onBackClick }) => {
     return (
       <div>
         <div>
-        <img src={movie.ImagePath} />
+        <img src={movie.ImagePath} style={{ width: "100%" }} />
       </div>
       <div>
         <span>Title: </span>
